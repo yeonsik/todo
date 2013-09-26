658ab3e1abf3db579750e9b3f93a9b76054b03fc
yeonsik <siktaeng@gmail.com>
2013-09-26 23:40:43 +0900

    주석 및 오타수정; 삭제기능추가(보완 필요함), 아이템 추가 로직 보완

--- js/main.js
+++ js/main.js
@@ -12,20 +12,33 @@ angular.module('todo')
     $scope.addPost = function() {
       $http.post("http://yeonsik-21519.apne1.actionbox.io:3000/items", { item: $scope.item })
         .success(function (data, status) {
-          $scope.items.push({done:false, title:$scope.item.title});
+          $scope.items.push(data);
           $scope.item.title = '';
         }).error(function (data, status) {
           $scope.status = status;
         });
     };
-    $scope.deletePost = function(item) {
-      $http.delete('hhttp://yeonsik-21519.apne1.actionbox.io:3000/items/'+item.id)
+    $scope.deletePost = function() {
+      angular.forEach($scope.items, function(value, key) {
+          if(value.done === true) {
+            $http.delete('http://yeonsik-21519.apne1.actionbox.io:3000/items/'+value.id)
+            .success(function (data, status) {
+                var index = $scope.items.indexOf(value);
+                $scope.items.splice(index, 1);
+              }).error(function (data, status) {
+                $scope.status = status;
+              });
+          }
+        });
+      /*
+      $http.delete('http://yeonsik-21519.apne1.actionbox.io:3000/items/'+item.id)
       .success(function (data, status) {
           var index = $scope.items.indexOf(post);
           $scope.items.splice(index, 1);
         }).error(function (data, status) {
           $scope.status = status;
         });
+    */
     };
     $scope.remain = function() {
         $scope.remainCount = 0;
